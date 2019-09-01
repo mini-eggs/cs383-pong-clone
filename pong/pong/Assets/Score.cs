469ca4bb49beac6ddbbb7b652508313cfc494a3e
Evan M Jones <me@evanjon.es>
2019-09-01 22:50:55 +0000

    Feat(help screen): Adding help screen before game begins.

---
+++ pong/pong/Assets/Score.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+  public static int PlayerOneScore = 0;
+  public static int PlayerTwoScore = 0;
+
+  /* Walls that when hit score a user
+   * a point.
+   * targetOne -> inc PlayerOneScore */
+  public string targetOne;
+  public string targetTwo;
+
+  /* max score 
+   * first to this wins game */
+  public int top;
+
+  /* Used to display either help screen on game
+   * Game is first put to help screen the user must press
+   * play to continue to game. */
+  private bool hasStarted;
+
+
+  /* fellow game object */
+  GameObject ball;
+
+  /* Make singleton, for accessing targetOne
+   * and targetTwo. There will *always* be one
+   * and only one of this instance alive. 
+   * Doing this due to Unity complaing about
+   * target(One|Two). */
+  public static Score self;
+
+  void Awake() {
+    self = this;
+    hasStarted = false;
+  }
+  
+  void Start() {
+    ball = GameObject.FindGameObjectWithTag("Ball");
+  }
+
+  /* so ball can tell when to stop moving */
+  public static bool IsOver() {
+    return PlayerOneScore >= self.top || PlayerTwoScore >= self.top;
+  }
+
+  public static void Hit(string wall) {
+    if(wall == self.targetOne) {
+      PlayerOneScore++;
+    }
+    else if(wall == self.targetTwo) {
+      PlayerTwoScore++;
+    }
+  }
+
+  // @override
+  // Not actually sure what calls this or why
+  // but here's where we can update points 
+  // and end of game messages/buttons.
+  // Oh nevermind, This is called like a ticker
+  // as `update` is.
+  void OnGUI() {
+    if(hasStarted) {
+      /* scores */
+      GUI.Label(new Rect(Screen.width / 2 - 140, 200, 150, 150), "Left Player: " + PlayerOneScore);
+      GUI.Label(new Rect(Screen.width / 2 + 50, 200, 150, 150), "Right Player: " + PlayerTwoScore);
+
+      /* directions */
+      GUI.Label(new Rect(Screen.width / 2 - 50, 460, 150, 150), "First to " + top + " wins!");
+
+      if(Score.IsOver()) {
+        /* show who won */
+        string msg = PlayerOneScore >= self.top ? "Left Player" : "Right Player";
+        GUI.Label(new Rect(Screen.width / 2 - 65, 300, 150, 150), "Congrats " + msg + "!");
+
+        /* give users a reset button */
+        if ( GUI.Button( new Rect( Screen.width / 2 - 50, 380, 100, 50 ), "Play Again!" ) ) {
+          PlayerOneScore = 0;
+          PlayerTwoScore = 0;
+          ball.SendMessage("QueueAnotherRound", 1.0f, SendMessageOptions.RequireReceiver);
+        }
+      }
+    }
+    else {
+      /* display help screen */
+      GUI.Label(new Rect(Screen.width / 2 - 150, 175, 500, 500), @"
+          HELP SCREEN
+
+          LEFT PLAYER CONTROLS:
+            Move up: 'w' key.
+            Move down: 's' key.
+          
+          RIGHT PLAYER CONTROLS:
+            Move up: '↑' arrow key.
+            Move down: '↓' arrow key.
+
+          RULES
+            First player to score five points wins.
+            A player scores a point by hitting the beach 
+            ball into the wall behind the other player.
+          ");
+
+      /* give users a reset button */
+      if ( GUI.Button( new Rect( Screen.width / 2 - 50, 425, 100, 50 ), "Begin!") ) {
+        hasStarted = true;
+        ball.SendMessage("QueueBegin", 1.0f, SendMessageOptions.RequireReceiver);
+      }
+    }
+  }
+}
