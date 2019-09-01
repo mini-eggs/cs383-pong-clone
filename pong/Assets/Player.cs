3d50411fee8c2b78d5e83efa3f934e9f3f94ed01
Evan M Jones <me@evanjon.es>
2019-09-01 22:00:24 +0000

    Feat(*): Breaking cs383 hw1 into its own public Git repo.

---
+++ pong/Assets/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+  // keys to respond to 
+  // configured via editor
+  public KeyCode up = KeyCode.W;
+  public KeyCode down = KeyCode.W;
+
+  // how fast our player moves
+  // configured via editor
+  public float speed;
+
+  // the arena our player can move (up/down)
+  // configured via editor
+  public float boundTop;
+  public float boundBottom;
+
+  private Rigidbody2D body;
+
+	void Start () {
+    body = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+    /* for player velocity */
+    var velo = body.velocity;
+    if(Input.GetKey(up)) {
+      velo.y = speed;
+    }
+    else if(Input.GetKey(down)) {
+      velo.y = speed * -1;
+    }
+    else {
+      velo.y = 0; /* no keys touched, stay still */
+    }
+
+    /* for position, don't let user exit boundY */
+    /* transform is given to use via superclass, MonoBehaviour */
+    var position = transform.position;
+    if(position.y > boundTop) {
+      position.y = boundTop;
+    }
+    else if(position.y < boundBottom) {
+      position.y = boundBottom;
+    }
+
+    /* update our player and transform with new vars */
+    body.velocity = velo;
+    transform.position = position;
+	}
+}
---
+++ pong/Assets/Wall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+  /* When we collide with something
+   * of this tag we'll count it as a point
+   * for one of the players */
+  public string triggerName;
+
+  // @override
+  void OnTriggerEnter2D(Collider2D item) {
+    if(item.name == triggerName) { /* the item that collided with us */
+      /* The ball has hit us!
+       * Send a message of our tag name
+       * (either East, or West) so the
+       * point can be given to the correct player. */
+
+      string name = transform.name; /* the wall name */
+      Score.Hit(name);
+
+      item.gameObject.SendMessage(
+          "QueueAnotherRound", 
+          1.0f, 
+          SendMessageOptions.RequireReceiver
+      );
+    }
+  }
+}
